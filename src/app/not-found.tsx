4c7ff0e2e4cc89e03b1c4a08756a6a9b58690218
Monique Domingues <moniqueadomingues@gmail.com>
2025-05-03 16:39:05 -0300

    adicionando pagina 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+  return (
+    <div className="">
+      Ooops! Página não encontrada
+        <br />
+      <a className="bg-blue-500" href="/" > Voltar para home</a>
+    </div>
+  );
+}
