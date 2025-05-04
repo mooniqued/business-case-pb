071b7e79abb931e04572effa7b37680f34536682
Monique Domingues <moniqueadomingues@gmail.com>
2025-05-04 20:40:34 -0300

    configurando layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,37 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Image from 'next/image';
 
-export const metadata: Metadata = {
-  title: "Consulta saque aniversário"
-};
+"use client";
+
+import "./globals.css";
+import { useState } from "react";
+import { FormProvider } from "./contextProvider";
+import Header from "./header";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [data, setData] = useState({
+    name: "", value: ""
+  });
+
   return (
     <html lang="en">
       <body
         className="flex flex-col min-h-screen overflow-hidden justify-center w-full items-center"
       >
-        <div className="absolute inset-0 bg-[url('/hero.jpg')] bg-cover bg-center bg-diagonal-left"></div>
-
-        <div className="absolute inset-0 bg-diagonal-right"></div>
-        <div className="relative z-10 flex flex-col w-[50%] gap-4">
-          <div className="flex gap-1 font-bold text-white items-center text-sm px-4">
-            <Image
-              src="/logoIcon.svg"
-              alt="logo"
-              width={20}
-              height={20}
-              className="text-white"
-            />
-            SMILE Co.
-          </div>
-          <div className="flex justify-around px-4"> 
-            <div className="italic text-white text-4xl font-bold w-[70%]">Use uma grana que já é sua e saia do aperto.</div>
-            <div>
-              <div className="uppercase text-white font-bold text-sm flex gap-2">
-                <Image
-                  src="/logoIcon.svg"
-                  alt="logo"
-                  width={20}
-                  height={20}
-                  className="text-white"
-                />
-                Saque aniversário
-              </div>
-              <div className="flex gap-2 text-white text-sm">
-                <div className="w-px bg-gray-300 " />
-                <span> <b>Insira seus dados</b> e verifique o quanto você poderá receber </span>
-              </div>
+        <section className="hero-diagonal">
+          <div className="hero__background w-full md:w-[50%]" >
+            <div className=" z-10 flex flex-col gap-4">
+              <FormProvider>
+                <Header />
+                <div className="bg-white rounded-xl w-full min-h-[200px] flex justify-around items-center items-center mt-6">
+                    {children}
+                </div>
+              </FormProvider>
             </div>
           </div>
-          <div className="bg-white rounded-xl min-h-[200px] flex justify-around items-center items-center">
-            {children}
-          </div>
-        </div>
+        </section>
       </body>
     </html>
   );
