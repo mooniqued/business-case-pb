6146c13e47ffab32743472c0b2fc1fb6a7672ed7
Monique Domingues <moniqueadomingues@gmail.com>
2025-05-04 20:39:32 -0300

    Página de resultado

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+import { FormContext } from "../context";
+import { useContext } from "react";
+
 export default function Result() {
-    return (
-      <div className="bg-grey rounded-xl h-[200px] flex justify-around items-center">
-        <div>
-            <span>Você pode receber até</span>
-            <span>R$ 13455,12</span></div>
-        <div></div>
+  const {data} = useContext(FormContext);
+
+  return (
+    <div className="rounded-xl h-[200px] flex flex-col md:flex-row justify-around items-center p-5">
+      <div className="w-full md:w-[35%] font-bold">
+        <span className="text-md text-[#20858c]">Você pode receber até</span>
+        <br />
+        <span>R$</span>
+        <span className="text-4xl text-[#20858c] pl-2">{data.value ? data.value : 0 } </span>
       </div>
-    );
-  }
+      <div className="w-full md:w-[32%] text-xs/4 font-bold text-[#747073]">
+        <span className="text-[#20858c]">Esta simulação traz valores aproximados.</span>
+        <span> Para calcular o valor exato,</span>
+        <span className="text-[#20858c]"> entre em contato com o Smile Co. a consultar seu saldo no app do FGTS.</span>
+      </div>
+    </div>
+  );
+}
+  
+
   
