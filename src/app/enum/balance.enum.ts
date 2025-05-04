54a32b491a7561a784886314838b1af040c70643
Monique Domingues <moniqueadomingues@gmail.com>
2025-05-04 20:40:00 -0300

    enum com dados para calculo do valor do saque

---
+++ src/app/enum/balance.enum.ts
@@ -0,0 +1,9 @@
+export const BALANCE = [
+  { max: 500, percentage: 0.5, aditional: 0  },
+  { max: 1000, percentage: 0.4, aditional: 50 },
+  { max: 5000, percentage: 0.3, aditional: 150 },
+  { max: 10000, percentage: 0.2, aditional: 650 },
+  { max: 15000, percentage: 0.15, aditional: 1150 },
+  { max: 20000, percentage: 0.1, aditional: 1900 },
+  { max: Infinity, percentage: 0.1, aditional: 1900 }
+]
